1eaed5e15bc0abb8722272ad64c24f1f572aebd4
Suvarna Kadam <suvarna.kadam@gmail.com>
2019-09-10 09:17:44 +0530

    source model generation for turn 1

--- model_selector.py
+++ model_selector.py
@@ -3,19 +3,20 @@ import os
 from learner import train_model
 from selector import evaluate_source_models
 #Train the 8 source models
-turn = 10
+turn = 1
 #base_dir = "D:\\phd\\experiments\\few-shot\\data\\miniImageNet\\images_background\\"
 base_dir = "/content/few-shot/data/miniImageNet/images_background/"
-# randomly pick 8 source concept labels for training
+# sequentially pick 8 source concept labels for training
 all_source_concepts = []
 for folder in os.listdir(base_dir):
 	all_source_concepts.append(folder)
 eight_source_concepts = all_source_concepts[(turn-1)*8:(turn-1)*8+8]
-# for concept in eight_source_concepts:
-#  	train_model(base_dir,concept)
+print("Source models trained",eight_source_concepts)
+for concept in eight_source_concepts:
+  	train_model(base_dir,concept)
 #load target concept
 base_target_dir = "/content/few-shot/data/miniImageNet/images_evaluation/"
-eight_source_concepts=['n02108089','n02138441','n03017168','n03075370','n03584254','n03770439','n03838899','n03998194']
+#eight_source_concepts=['n02108089','n02138441','n03017168','n03075370','n03584254','n03770439','n03838899','n03998194']
 #base_target_dir = "D:\\phd\\experiments\\few-shot\\data\\miniImageNet\\images_evaluation\\"
 all_target_concepts = []
 for folder in os.listdir(base_target_dir):
